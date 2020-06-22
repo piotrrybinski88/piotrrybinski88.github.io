86e116a9f85b5155c47ba8fd3ec4e576c92429cb
Piotr Rybiński 1 <piotr.rybinski.1@lingarogroup.com>
2020-06-22 16:46:01 +0200

    added second blog post

---
+++ content/second-post.md
@@ -0,0 +1,7 @@
+title: Second Post
+date: 22-06-2020
+author: Piotr
+
+# Python and more Post 2.
+
+How to use Docker and why.
--- pelicanconf.py
+++ pelicanconf.py
@@ -10,7 +10,7 @@ PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
 
-DEFAULT_LANG = 'English'
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
